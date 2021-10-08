8e43dbfee30f9f3307bba30c35522ed35db7c9e0
Andreas Manos <antmanos42@gmail.com>
2021-10-09 01:04:00 +0200

    created author entity and repository, also dummy write test

--- src/main/java/com/bafdev/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/com/bafdev/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -1,12 +1,16 @@
 package com.bafdev.betterreadsdataloader;
 
+import com.bafdev.betterreadsdataloader.author.Author;
+import com.bafdev.betterreadsdataloader.author.AuthorRepository;
 import com.bafdev.betterreadsdataloader.connection.DataStaxAstraProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Path;
 
 @SpringBootApplication
@@ -15,6 +19,14 @@ public class BetterreadsDataLoaderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+    }
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @PostConstruct
+    public void start() {
+
     }
 
     @Bean
